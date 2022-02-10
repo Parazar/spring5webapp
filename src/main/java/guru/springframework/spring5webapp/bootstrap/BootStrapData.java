8ec82ad8380a3093c055c7a9b39f4d72e5967446
Onimiste <theo.hipault@gmail.com>
2022-02-10 11:18:28 +0100

    Course 18: Added publisher-book relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,13 +25,21 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher dialPress = new Publisher("Dial Press", "Somewhere", "New York City", "US", "Not a RAR");
+        publisherRepository.save(dialPress);
+
         Author annieBarrows = new Author("Annie", "Barrows");
         Book theTruth = new Book("The Truth According to Us", "45587");
         annieBarrows.getBooks().add(theTruth);
         theTruth.getAuthors().add(annieBarrows);
 
+        theTruth.setPublisher(dialPress);
+        dialPress.getBooks().add(theTruth);
+
         authorRepository.save(annieBarrows);
         bookRepository.save(theTruth);
+        publisherRepository.save(dialPress);
 
         Author maryAnnShaffer = new Author("May Ann", "Shaffer");
         Book potatoPeelPie = new Book("The Guernsey Literary and Potato Peel Pie Society", "1234500");
@@ -39,13 +47,15 @@ public class BootStrapData implements CommandLineRunner {
         potatoPeelPie.getAuthors().add(maryAnnShaffer);
         potatoPeelPie.getAuthors().add(annieBarrows);
 
+        potatoPeelPie.setPublisher(dialPress);
+        dialPress.getBooks().add(potatoPeelPie);
+
         authorRepository.save(maryAnnShaffer);
         bookRepository.save(potatoPeelPie);
-
-        Publisher dialPress = new Publisher("Dial Press", "Somewhere", "New York City", "US", "Not a RAR");
         publisherRepository.save(dialPress);
 
         System.out.println("Started in Bootstrap");
+        System.out.println("Number of Publishers: " + publisherRepository.count());
         System.out.println("Number of Books: " + bookRepository.count());
     }
 }
